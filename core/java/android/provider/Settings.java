bbb4a8190fa1a4d4b39bafd7cdeebe551dc978d6
ezio84 <brabus84@gmail.com>
2020-11-08 16:19:23 +0100

    Show Ambient instead of lockscreen on wake gestures [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    * Drop soli thingy
    
    Change-Id: I11b0ad51353550d585d4db7d30b983bf551a53a4
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5689,6 +5689,13 @@ public final class Settings {
          */
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
 
+        /** Whether to show ambient or lockscreen if AoD is disabled
+         * and we do a wake gesture like lift to wake or double tap
+         *
+         * @hide
+         */
+        public static final String AMBIENT_WAKE_GESTURES = "ambient_wake_gestures";
+
         /**
          * Enable statusbar double tap gesture to put device to sleep
          * @hide
