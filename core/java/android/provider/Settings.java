64812387515c87d237d827775d4ed92f5d761d3d
SagarMakhar <sagarmakhar@gmail.com>
2021-08-28 14:15:37 +0000

    base: Allow locking tasks to recents [1/2]
    Change-Id: Id18af94cf52db46054299a862b91c739e8d204f6
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5642,6 +5642,11 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
+         * @hide
+         */
+        public static final String RECENTS_LOCKED_TASKS = "recents_locked_tasks";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
