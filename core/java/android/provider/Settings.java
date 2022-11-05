4ad90ebc1b9da691b7bf9e1ea1b616aae0180b5b
ezio84 <brabus84@gmail.com>
2022-11-05 12:06:20 +0000

    Allow doubletap/longpress power to toggle torch [1/2]
    AEX Edits:
    - Port to S
    - handle camera gesture here instead of extensions
    - simplify code
    
    Thanks to beanstown106 for the initial longpress action calls in
    PhoneWindowManager (improved by lineage guys)
    
    If the user enables double tap for camera action, torch action will
    automatically switch to long press mode.
    If the user enables double tap for torch, double tap for camera
    will be disabled accordingly.
    
    Remove the settings pref is the device doesn't have flashlight
    
    A 170ms delay for double tap action is not annoying on single tap
    to wake up the device.
    
    Vibrate when toggling torch to let the user know of unwanted toggling
    in the pocket (even if vibration is disabled by user)
    
    Allow torch action also on ambient display
    
    mydongistiny: Fixed for android10
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5677,6 +5677,17 @@ public final class Settings {
         public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
 
         /**
+         * Whether the torch launch gesture to double tap or long press the power button when the
+         * screen is off should be enabled. *
+         * 0: disabled
+         * 1: double tap power for torch
+         * 2: long tap power for torch
+         * @hide
+         */
+        @Readable
+        public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
